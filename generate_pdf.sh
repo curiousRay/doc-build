86a7ac62b707976c3ff48ad45603bea250215d8a
curiousRay <leilei199708@gmail.com>
2024-01-25 20:28:49 +0800

    modify layout

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -6,6 +6,7 @@ sudo docker run --rm \
        	--volume "$(pwd):/data" \
        	--user $(id -u):$(id -g) \
        	nightkeeper:v1 test.md -o test.pdf --template eisvogel \
+        --toc \
        	--listings \
         --pdf-engine "xelatex" \
     -V CJKmainfont="Source Han Sans SC" \
@@ -15,7 +16,13 @@ sudo docker run --rm \
     -V titlepage-rule-height=0 \
     -V titlepage-background="background.pdf" \
     -V author="lihao.lei" \
+    -V author-meta="lihao.lei" \
+    -V title-meta="Instruction For Use" \
     -V date="80000000-ETD-ENG-01-20240121" \
     -V titlepage-logo="logo.pdf" \
-    -V logo-width="30mm"
-
+    -V logo-width="30mm" \
+    -V toc-own-page="true" \
+    -V geometry:top="2.5cm" \
+    -V geometry:bottom="2.5cm" \
+    -V geometry:left="1.5cm" \
+    -V geometry:right="1.5cm"
